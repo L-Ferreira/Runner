5f4a10200ca28ff8619ce838b827eaf7cccae60a
Luís Ferreira <luispaulo_26@hotmail.com>
2020-07-07 23:21:18 +0100

    Feito ate ao video 12 (inclusive)

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public PlayerController thePlayer;
+
+    private Vector3 lastPlayerPosition;
+    private Vector3 targetPosition;
+    private Vector3 cameraVelocity;
+    private float distanceToMove;
+    private float smoothTime = 0.0001f;
+
+    void Awake () {
+        thePlayer = FindObjectOfType<PlayerController> ();
+        lastPlayerPosition = thePlayer.transform.position;
+    }
+
+    void LateUpdate () {
+        distanceToMove = thePlayer.transform.position.x - lastPlayerPosition.x;
+
+        targetPosition = new Vector3 (transform.position.x + distanceToMove, transform.position.y, transform.position.z);
+        transform.position = Vector3.SmoothDamp (transform.position, targetPosition, ref cameraVelocity, smoothTime);
+
+        lastPlayerPosition = thePlayer.transform.position;
+    }
+}
---
+++ Assets/Scripts/PlatformGenerator.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformGenerator : MonoBehaviour {
+
+    public GameObject thePlatform;
+    public Transform generationPoint;
+    public float distanceBetween;
+
+    private float platformWidth;
+
+    public float distanceBetweenMin;
+    public float distanceBetweenMax;
+
+    // public GameObject[] thePlatforms;
+    private int platformSelector;
+    private float[] platformWidths;
+
+    public ObjectPooler[] theObjectPools;
+
+    private float minHeight;
+    private float maxHeight;
+    public Transform maxHeightPoint;
+    public float maxHeightChange;
+    private float heightChange;
+
+    void Start () {
+        // platformWidth = thePlatform.GetComponent<BoxCollider2D> ().size.x;
+        platformWidths = new float[theObjectPools.Length];
+
+        for (int i = 0; i < theObjectPools.Length; i++) {
+            platformWidths[i] = theObjectPools[i].pooledObject.GetComponent<BoxCollider2D> ().size.x;
+        }
+
+        minHeight = transform.position.y;
+        maxHeight = maxHeightPoint.position.y;
+
+    }
+
+    void Update () {
+        if (transform.position.x < generationPoint.position.x) {
+
+            distanceBetween = Random.Range (distanceBetweenMin, distanceBetweenMax);
+
+            platformSelector = Random.Range (0, theObjectPools.Length);
+
+            heightChange = transform.position.y + Random.Range (maxHeightChange, -maxHeightChange);
+
+            if (heightChange > maxHeight) {
+                heightChange = maxHeight;
+            } else if (heightChange < minHeight) {
+                heightChange = minHeight;
+            }
+
+            transform.position = new Vector3 ((transform.position.x + platformWidths[platformSelector] / 2) + distanceBetween, heightChange, transform.position.z);
+
+            //Instantiate (thePlatforms[platformSelector], transform.position, transform.rotation);
+            GameObject newPlatform = theObjectPools[platformSelector].GetPooledObject ();
+            newPlatform.transform.position = transform.position;
+            newPlatform.transform.rotation = transform.rotation;
+            newPlatform.SetActive (true);
+
+            transform.position = new Vector3 ((transform.position.x + platformWidths[platformSelector] / 2), transform.position.y, transform.position.z);
+
+        }
+
+    }
+}
